eb5e471d0e66019d8d20bb9081702b38b42c5e41
zzz <kkzzhizhou@gmail.com>
2020-05-04 22:10:59 +0800

    Update Mon May  4 22:10:59 CST 2020

--- scripts/spicetify-apply.ps1
+++ scripts/spicetify-apply.ps1
@@ -1,5 +1,7 @@
 $spotify_running = Get-Process -ErrorAction Ignore -Name Spotify
 Stop-Process -ErrorAction Ignore -Name Spotify
-& "$PSScriptRoot\spicetify.exe" restore backup apply --quiet --no-restart
+# restore should be run separately because if there is no backup, it will error and exit
+& "$PSScriptRoot\spicetify.exe" restore --quiet --no-restart
+& "$PSScriptRoot\spicetify.exe" backup apply --quiet --no-restart
 if (Get-Command "blockthespot" -ErrorAction Ignore) { blockthespot }
 if ($spotify_running) { Start-Process "$(& `"$PSScriptRoot\get-spotify-path.ps1`")" }
