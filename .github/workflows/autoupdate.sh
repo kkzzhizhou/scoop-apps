b2e48464275937a85b6037676f5faa7f9a2926ad
Jozy <806508401@qq.com>
2021-06-23 09:45:50 +0800

    Update autoupdate.sh

--- .github/workflows/autoupdate.sh
+++ .github/workflows/autoupdate.sh
@@ -29,7 +29,7 @@ do
     if [ ! -d "cache/$bucket_dir" ]
     then
         echo "clone bucket:$bucket"
-        git clone --depth==1 https://github.com/$bucket cache/$bucket_dir
+        git clone --depth=1 https://github.com/$bucket cache/$bucket_dir
     fi
 done
 
