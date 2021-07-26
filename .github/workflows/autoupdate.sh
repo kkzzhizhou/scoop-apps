b23fcd6baea3db0750af1bc094b60db802cef6ac
zzz <806508401@qq.com>
2021-07-26 02:14:15 +0000

    feature: 识别md5重复文件

--- .github/workflows/autoupdate.sh
+++ .github/workflows/autoupdate.sh
@@ -27,22 +27,22 @@ for bucket in ${buckets[@]}; do
         echo "$bucket_name" >> bucket.config
     fi
 done
+
 buckets=$(cat bucket.config)
 script_buckets=$(tac bucket.config)
 confuses=$(cat $script_dir/app.confuse)
 
-# check env
-echo "check cache dir"
-[ -d "cache" ] || mkdir -p cache
+# create cache dir
+cache_dir=`mktemp -d`
 
 # download bucket
 for bucket in ${buckets[@]}
 do
     bucket_dir=$(echo $bucket | sed 's@/@-@g')
-    if [ ! -d "cache/$bucket_dir" ]
+    if [ ! -d "${cache_dir}/$bucket_dir" ]
     then
         echo "clone bucket:$bucket"
-        git clone --depth=1 https://github.com/$bucket cache/$bucket_dir
+        git clone --depth=1 https://github.com/$bucket ${cache_dir}/$bucket_dir
     fi
 done
 
@@ -51,42 +51,54 @@ rm -rf scripts/*
 for bucket in ${script_buckets[@]}
 do
     bucket_dir=$(echo $bucket | sed 's@/@-@g')
-    if [ -d "cache/${bucket_dir}/scripts" ]
+    if [ -d "${cache_dir}/${bucket_dir}/scripts" ]
     then
-        cp -rf cache/${bucket_dir}/scripts/* ./scripts/
+        cp -rf ${cache_dir}/${bucket_dir}/scripts/* ./scripts/
     fi
 done
 
-
 # merge bucket
 rm -f bucket/*.json
-rm -f cache/file_ids && touch cache/file_ids
+rm -f ${cache_dir}/file_ids && touch ${cache_dir}/file_ids
+rm -f ${cache_dir}/file_md5 && touch ${cache_dir}/file_md5
 rm -f app-contributor-list.txt
 echo "name    bucket" > app-contributor-list.txt
 for bucket in ${buckets[@]}
 do
     bucket_dir=$(echo $bucket | sed 's@/@-@g')
     echo $bucket_dir
-    files=$(find cache/$bucket_dir -type f -name *.json -not -path "cache/$bucket_dir/.vscode/*")
+    files=$(find ${cache_dir}/${bucket_dir} -type f -name *.json -not -path "${cache_dir}/$bucket_dir/.vscode/*")
     for file in ${files[@]}
     do
         file_name=$(echo $file | awk -F'/' '{print $NF}')
         file_id=$(echo $file_name | tr 'A-Z' 'a-z')
-        check_file_id=$(cat cache/file_ids | grep -E "^$file_id$" | wc -l)
+        file_md5=$(md5sum $file | awk '{print $1}')
+        check_file_id=$(cat ${cache_dir}/file_ids | grep -E "^$file_id$" | wc -l)
+        check_file_md5=$(cat ${cache_dir}/file_md5 | grep -E "^$file_md5$" | wc -l)
         if [ "$check_file_id" -eq 0 ]
         then
             # record file_id
-            echo $file_id >> cache/file_ids
+            echo $file_id >> ${cache_dir}/file_ids
+            # record file_md5
+            echo $file_md5 >> ${cache_dir}/file_md5
+            # copy file to bucket
             cp -f $file ./bucket/$file_name
-	    # record app-contributor-list.txt
-	    echo "$file_name    $bucket" >> app-contributor-list.txt
-        else
+	        # record app-contributor-list.txt
+	        echo "$file_name    $bucket" >> app-contributor-list.txt
+        elif [ "$check_file_md5" -eq 0 ]
+        then
             # rename file
             owner=$(echo $bucket | awk -F'/' '{print $1}')
             new_name=$(echo $file_name | sed "s/.json/_$owner.json/")
+            # record file_md5
+            echo $file_md5 >> ${cache_dir}/file_md5
+	        # record app-contributor-list.txt
+	        echo "$new_name    $bucket" >> app-contributor-list.txt
+            # copy file to bucket
             cp -f $file ./bucket/$new_name
-	    # record app-contributor-list.txt
-	    echo "$new_name    $bucket" >> app-contributor-list.txt
+        else
+           # ignore duplicate file
+           echo "duplicate file:$bucket   $file"
         fi
     done
 done
@@ -103,7 +115,6 @@ do
 done
 
 # update README.md
-
 sed -i '/^# 合并仓库列表/,$d' README.md
 echo -e "# 合并仓库列表\n" >> README.md
 for bucket in ${buckets[@]}
